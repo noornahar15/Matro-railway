942f25f4b875841d7a1a5ffaef3e5b87e1f7d522
noornahar15 <nrsikder2@gmail.com>
2023-09-02 02:06:11 +0600

    fun

--- MetroRailway/app/src/main/java/com/nahar/metrorailway/ui/theme/Dashboard.kt
+++ MetroRailway/app/src/main/java/com/nahar/metrorailway/ui/theme/Dashboard.kt
@@ -1,13 +1,21 @@
 package com.nahar.metrorailway.ui.theme
 
 import android.os.Bundle
+import android.widget.LinearLayout
 import com.nahar.metrorailway.R
 import androidx.activity.ComponentActivity
 
 class Dashboard : ComponentActivity() {
+     lateinit var ticketLayoutId: LinearLayout
      override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+         init()
+    }
+
+    private fun init(){
+        ticketLayoutId = findViewById(R.id.ticketLayoutId)
     }
 }
 
